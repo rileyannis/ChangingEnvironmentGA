2bd7de07c92d450da33a062e9ba532468b05cad2
Joshua Nahum <josh@nahum.us>
2014-09-17 11:27:42 -0400

    made some actual code

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
+
+import random
+population = []
+for _ in range(100):
+    org = random.randrange(1, 101)
+    population.append(org)
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    print(population)
