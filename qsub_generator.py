32a284b0e4a48129531ddd998a5996a7874705eb
Joshua Nahum <josh@nahum.us>
2014-11-26 23:31:42 -0500

    Refactored and improved qsub generator

--- qsub_generator.py
+++ qsub_generator.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 from subprocess import call
 
 array_size = 10
@@ -21,11 +22,8 @@ module load SciPy
 cd ~/ChangingEnvironmentGA
 """.format(array_size)
 
-output_dir_base = "junkdata"
-config_files = ["test1_config.ini", "test2_config.ini"]
-jobs = [convert_config_file_name_to_job(name, output_dir_base) for name in config_files]
 
-config_commonn = """
+config_common = """
 [DEFAULT]
 fitness_function_type = {0}
 number_of_organisms = 500
@@ -47,17 +45,24 @@ def write_to_file(filename, contents):
     with open(filename, "w") as file_handle:
         file_handle.write(contents)
 
-for function_type, alt_corr in itertools.product(function_types, alt_corrs):
-    config_filename = "{}_{}.ini".format(function_type, alt_corr)
-    contents = config_common.format(function_type, alt_corr)
-
 
 if __name__ == "__main__":
+    config_files = []
+
+    for function_type, alt_corr in itertools.product(function_types, alt_corrs):
+        config_filename = "{}_{}.ini".format(function_type, alt_corr)
+        contents = config_common.format(function_type, alt_corr)
+        write_to_file(config_filename, contents)
+        config_files.append(config_filename)
+
+    output_dir_base = "junkdata1"
+    jobs = [convert_config_file_name_to_job(name, output_dir_base) for name in config_files]
+
+
     for job in jobs:
-        with open("tmp.sub", 'w') as f:
-            f.write(common)
-        f.write(job + '\n')
+        contents = common + "\n" + job + "\n"
+        write_to_file("tmp.sub", contents)
         print "----------Submitting----------"
-    print common + job
-    call(["qsub", 'tmp.sub'])
-    call(["rm", 'tmp.sub']) 
+        print contents
+        call(["qsub", 'tmp.sub'])
+        call(["rm", 'tmp.sub']) 
