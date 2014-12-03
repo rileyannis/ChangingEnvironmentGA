6da306857b3a0c27bb5d3d246e98eb995114172c
Riley Jameson Annis <annisril@msu.edu>
2014-12-03 14:48:04 -0500

    finished (I hope) unittests for string_org

--- string_org.py
+++ string_org.py
@@ -1,7 +1,7 @@
 import random
 
-TARGET_STRING = "aaabbc"
-LETTERS = "abcd"
+TARGET_STRING = None
+LETTERS = None
 
 class StringOrg(object):
     """
@@ -13,18 +13,25 @@ class StringOrg(object):
 
     def __init__(self, genotype=None):
         if genotype is None:
-            genotype = create_random_genotype()
+            genotype = _create_random_genotype()
         assert len(genotype) == len(TARGET_STRING)
         self.genotype = genotype
 
-    def get_fitness(self):
-        return get_fitness(self.genotype)
+    def get_fitness(self, environment):
+        return environment(self.genotype)
 
     def get_mutant(self):
-        return StringOrg(get_mutated_genotype(self.genotype))
+        return StringOrg(_get_mutated_genotype(self.genotype))
 
     def __str__(self):
+        print("in str, genotype is", self.genotype)
         return "StringOrg(\"{}\")".format(self.genotype)
+
+    def __eq__(self, other):
+        return self.genotype == other.genotype
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __repr__(self):
         return str(self)
@@ -32,7 +39,10 @@ class StringOrg(object):
     def get_clone(self):
         return StringOrg(self.genotype)
 
-def get_mutated_genotype(genotype):
+    def is_better_than(self, other, environment):
+        return self.get_fitness(environment) > other.get_fitness(environment)
+
+def _get_mutated_genotype(genotype):
     "Mutates one character in organism at random"
     mut_location = random.randrange(len(TARGET_STRING))
     while True:
@@ -40,15 +50,15 @@ def get_mutated_genotype(genotype):
         if mut_letter != genotype[mut_location]:
             return genotype[:mut_location] + mut_letter + genotype[mut_location + 1:]
 
-def get_fitness(org):
+def default_environment(genotype):
     "Returns the number of matching letters between org and target"
     matches = 0
     for i in range(len(TARGET_STRING)):
-        if TARGET_STRING[i] == org[i]:
+        if TARGET_STRING[i] == genotype[i]:
             matches += 1
     return matches
 
-def create_random_genotype():
+def _create_random_genotype():
     genotype = []
     for _ in range(len(TARGET_STRING)):
         genotype.append(random.choice(LETTERS))
