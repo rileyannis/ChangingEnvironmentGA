c8a9e3bb2871e64abfcc55475200dc4c1c84162d
Riley Jameson Annis <annisril@msu.edu>
2015-02-11 11:05:22 -0500

    started profiling

--- changing_environment_ga.py
+++ changing_environment_ga.py
@@ -5,13 +5,13 @@ import sys
 from string import ascii_uppercase
 import time 
 
-def parse_everything():
+def parse_everything(command_line_args=None):
     conf_parser = argparse.ArgumentParser(
         description='The changing environments program.')
     conf_parser.add_argument("-c", "--config_file", nargs=1)
-    conf_parser.add_argument("-o", '--output_folder', nargs=1)
-
-    args = conf_parser.parse_args()
+    conf_parser.add_argument("-o", "--output_folder", nargs=1)
+    
+    args = conf_parser.parse_args(args=command_line_args)    
 
     output_folder = args.output_folder[0]
     config_file = args.config_file[0]
@@ -24,8 +24,10 @@ def parse_everything():
     config.set("DEFAULT", "start_time", str(time.time()))
     return config
 
-def main():
-    config = parse_everything()
+def main(command_line_args=None):
+    if command_line_args is not None:
+        command_line_args = command_line_args.split()
+    config = parse_everything(command_line_args)
     set_global_variables(config)
     generate_data()
 
---
+++ profile.py
@@ -0,0 +1,7 @@
+import cProfile
+import main
+import changing_environment_ga
+
+args = "\"-c vector_config.ini -o profile_junk_data\""
+
+cProfile.run("changing_environment_ga.main(" + args + ")")
