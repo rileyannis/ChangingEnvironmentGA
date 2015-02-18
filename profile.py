c5b807e644de700e7a240c130cb37109cf3816fe
Riley Jameson Annis <annisril@msu.edu>
2015-02-18 12:18:49 -0500

    made profiling sort by time

--- profile.py
+++ profile.py
@@ -1,7 +1,8 @@
 import cProfile
+import pstats
 import main
 import changing_environment_ga
 
 args = "\"-c vector_config.ini -o profile_junk_data\""
 
-cProfile.run("changing_environment_ga.main(" + args + ")")
+cProfile.run("changing_environment_ga.main(" + args + ")", None, "time")
