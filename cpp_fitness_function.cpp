4d7a2d68a8e8ee5ea1ab3a4bd484d40270f2bebe
rileyannis <annisril@msu.edu>
2015-04-22 11:43:38 -0400

    Commented cpp fitness function

--- cpp_fitness_function.cpp
+++ cpp_fitness_function.cpp
@@ -5,6 +5,7 @@ using std::accumulate;
 #include<cmath>
 using std::pow; using std::sqrt; using std::sin; using std::cos; using std::abs;
 
+//This was just for testing arrays
 double add_array(double* ary, int sz){
   double sum = 0;
   for(int i = 0; i < sz; ++i){
@@ -14,18 +15,22 @@ double add_array(double* ary, int sz){
 }
 
 float cpp_flat_function(vector<float> vals){
+  //Evaluates on a flat landscape. Should probably remove and just return 0.0 in cython
   return 0.0;
 }
 
 float inline adding_squares(float tot, float val){
+  //Inline function made for accumulate in sphere_function
   return tot + (val*val);
 }
 
 float cpp_sphere_function(vector<float> vals){
+  //Accumulates the squares of all vals
   return accumulate(vals.begin(), vals.end(), 0.0, adding_squares);
 }
 
 float cpp_rosenbrock_function(vector<float> vals){
+  //The rosenbrock function! It does things!
   float tot = 0.0;
   for(unsigned int i = 0; i < (vals.size() - 1); ++i){
     tot += (100.0 * pow(vals[i+1] - (pow(vals[i], 2)), 2) + pow(vals[i] - 1, 2));
@@ -34,6 +39,7 @@ float cpp_rosenbrock_function(vector<float> vals){
 }
 
 float cpp_rana_function(vector<float> vals, vector<float> weights){
+  //The rana function! It does more things! Wow!
   float tot = 0.0, x, y;
   for(unsigned int i = 0; i < vals.size(); ++i){
     x = vals[i];
@@ -50,6 +56,7 @@ float cpp_rana_function(vector<float> vals, vector<float> weights){
 }
 
 float cpp_schafferF7(vector<float> vals){
+  //The schafferF7 function! It does other things!
   float tot = 0.0, norm = 1.0/vals.size(), si;
   for(unsigned int i = 0; i < (vals.size() - 1); ++i){
     si = sqrt(pow(vals[i], 2) + pow(vals[i + 1], 2));
@@ -59,6 +66,7 @@ float cpp_schafferF7(vector<float> vals){
 }
 
 float cpp_deceptive(vector<float> vals){
+  //The deceptive function. It does stuff.
   float decep = 0.20, best = 1.0, decep_best = 0.7, tot = 0.0;
   int dim = vals.size();
   for(int i = 0; i < dim; ++i){
