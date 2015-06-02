155d7a67d5a972d7bf976675adce1353f16a7f91
Mikaela Leas <mikaela.leas@gmail.com>
2015-06-02 13:14:53 -0400

    Working on Bit Vector Genotype

--- bit_vector_org.py
+++ bit_vector_org.py
@@ -1,5 +1,120 @@
-"""This file is not need for real value vectors.
-It's just here so main won't complain."""
+"""
 
-class BitVectorOrg:
-    pass
+"""
+
+import random
+
+MAX_BITS_OF_MEMORY = None
+
+class MemoryPDGenotype(object):
+    """
+    """
+
+    def __init__(self, number_of_bits_of_memory, decision_list):
+        assert len(decision_list) == 2 ** number_of_bits_of_memory
+        self.number_of_bits_of_memory = number_of_bits_of_memory
+        self.decision_list = decision_list
+
+    def get_mutant_of_self(self):
+        """
+        Returns a mutant of the current genotype.
+        Mutations are as equally likely to happen to any single location in the decision 
+        list as to change the number of bits of memory.
+        Thus, the number of possible mutations is the length of the decision list plus one
+        (possibility of changing length).
+        """
+
+        def get_bits_of_memory_mutant():
+            should_increase_memory = random.choice([True, False])
+            if self.number_of_bits_of_memory == 0 and not should_increase_memory:
+                return self
+            if self.number_of_bits_of_memory == MAX_BITS_OF_MEMORY and should_increase_memory:
+                return self
+            if should_increase_memory:
+                new_number_of_bits_of_memory = self.number_of_bits_of_memory + 1
+                new_decision_list = self.decision_list * 2
+                return MemoryPDGenotype(new_number_of_bits_of_memory, new_decision_list)
+            # should decrease memory
+            new_number_of_bits_of_memory = self.number_of_bits_of_memory - 1
+            length_of_new_decision_list = len(self.decision_list) // 2
+            new_decision_list = self.decision_list[:length_of_new_decision_list]
+            return MemoryPDGenotype(new_number_of_bits_of_memory, new_decision_list)
+
+        def decision_list_mutant(mutation_location):
+            new_decision_list = self.decision_list[:]
+            new_decision_list[mutation_location] = not new_decision_list[mutation_location]
+            return MemoryPDGenotype(self.number_of_bits_of_memory, new_decision_list)
+
+        mutation_location = random.randrange(len(self.decision_list) + 1)
+        if mutation_location == len(self.decision_list):
+            return get_bits_of_memory_mutant()
+        return decision_list_mutant(mutation_location)
+
+
+class BitVectorOrg(object):
+    """
+        
+        
+        
+    """
+    
+    def __init__(self, genotype=None):
+        if genotype is None:
+            genotype = _create_random_genotype()
+        else:
+            genotype = np.asarray(genotype, dtype=np.float64)
+        self.genotype = genotype
+        self._fitness_cache = None
+    
+    def fitness(self, environment):
+        if self._fitness_cache is None:
+            self._fitness_cache = environment(self.genotype)
+        return self._fitness_cache
+    
+    def reset_fitness_cache(self):
+        self._fitness_cache = None
+    
+    def get_mutant(self):
+        return RealValueVectorOrg(_get_mutated_genotype(self.genotype, MUTATION_EFFECT_SIZE))
+    
+    def __eq__(self, other):
+        return self.genotype == other.genotype
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __str__(self):
+        return "RealValueVectorOrg({})".format(self.genotype)
+    
+    def __repr__(self):
+        return str(self)
+    
+    def is_better_than(self, other, environment):
+        return self.fitness(environment) < other.fitness(environment)
+
+def _get_mutated_genotype(genotype, effect_size):
+    """Mutates one locus in organism at random"""
+    mut_location = random.randrange(genotype.shape[0])
+    delta = random.normalvariate(0, effect_size)
+    mutant_value = genotype[mut_location] + delta
+    #Ensure a copy is made so mutant doesn't edit the original
+    mutant = np.array(genotype, copy=True)
+    mutant[mut_location] = _wrap_around(mutant_value, RANGE_MIN, RANGE_MAX)
+    return mutant
+
+def _wrap_around(value, min_, max_):
+    """Literally does what it says."""
+    width = max_ - min_
+    while value < min_ or value > max_:
+        if value < min_:
+            value += width
+        else:
+            value -= width
+    return value
+
+def _create_random_genotype():
+    """Create a random array genotype"""
+    genotype = np.zeros(LENGTH, dtype=np.float64)
+    for i in range(LENGTH):
+        genotype[i] = random.uniform(RANGE_MIN, RANGE_MAX)
+    return genotype
