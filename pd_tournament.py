17db9d52f722d398a97054e8d655005439e93ea6
Mikaela Leas <mikaela.leas@gmail.com>
2015-06-03 17:17:02 -0400

    Working on fitness function pd_tournament

--- pd_tournament.py
+++ pd_tournament.py
@@ -12,6 +12,8 @@ REWARD = 3
 PUNISHMENT = 1
 SUCKER = 0
 
+PROPORTION_COST_PER_MEMORY_BIT = .01
+
 def pd_payout(a_cooperates, b_cooperates):
     """
     Function my_reward determines reward given by the state of self and other
@@ -37,9 +39,6 @@ def pd_payout(a_cooperates, b_cooperates):
     raise AssertionError("Impossible To Get Here")
 
 
-
-
-
 def run_game(organism_a, organism_b):
     """
     Run a game of NUMBER OF ROUNDS long
@@ -61,4 +60,22 @@ def run_game(organism_a, organism_b):
         total_payout_b += payout_b
     
     return total_payout_a, total_payout_b
+    
+def get_relative_fitness(organism_a, organism_b):
+    def proportion_cost(org):
+        return PROPORTION_COST_PER_MEMORY_BIT * org.genotype.number_of_bits_of_memory
+    
+    def get_adjusted_payout(org, payout):
+        pass
+        
+    payout_a, payout_b = run_game(organism_a, organism_b)
+    
+    a_proportion_cost = proportion_cost(organism_a)
+    b_proportion_cost = proportion_cost(organism_b)
+    
+    
+    adj_payout_a = payout_a * (1 - a_proportion_cost)
+    adj_payout_b = payout_b * (1 - b_proportion_cost)
+    
+    
     
