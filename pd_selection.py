d94fb903e2deb77d01631595d9f3554c95478197
Mikaela Leas <mikaela.leas@gmail.com>
2015-06-04 16:56:38 -0400

    Working on next_generation

--- pd_selection.py
+++ pd_selection.py
@@ -1,4 +1,7 @@
 import pd_tournament
+import random
+
+TOURNAMENT_SIZE = None
 
 def get_best_half(paired_organism_payouts):
     """
@@ -9,6 +12,36 @@ def get_best_half(paired_organism_payouts):
     best_half_pairs = sorted_pairs[:len(paired_organism_payouts) // 2]
     return [org for org, _ in best_half_pairs]
     
-
+def get_next_generation_by_selection(organisms):
+    """
+    Get next generation by selection is a function that takes a list of organisms
     
+    Shuffle the organisms and group them into TOURNAMENT_SIZEd clumps
+    For a given tournament size, it runs a tournament
+    selects the best half
+    Adds them to a population that becomes the next generation
+    If more tournaments more need to be run after all the clumps are used, we shuffle the
+    population and make more clumps
+    It repeats more tournaments until the next generation reaches the population size
+    """
+    def generate_contenders(organisms):
+        random.shuffle(organisms)
+        number_of_tournaments = len(organisms) // TOURNAMENT_SIZE
+        if not len(organisms) % TOURNAMENT_SIZE:
+            number_of_tournaments += 1
+        for i in range(number_of_tournaments):
+            organisms[TOURNAMENT_SIZE * i: TOURNAMENT_SIZE * (i + 1)]
+            
+            
+            
+            
+    next_generation = []
+    #function that adds things to next generation
+    while len(next_generation) < len(organisms):
+        #pick organisms for the tournament
+        
+        #get the winners of the tournament
+        #add winners to the next generation
+    #make next_generation same length as organisms
+    return next_generation[:len(organisms)]
     
--- test_pd_selection.py
+++ test_pd_selection.py
@@ -1,8 +1,10 @@
 import unittest
 import pd_selection
+import pd_org
 
 class TestSelection(unittest.TestCase):
     def setUp(self):
+        pd_org.MAX_BITS_OF_MEMORY = 5
         self.paired_organism_payouts = [('org',   2.15), 
                                         ('org2',  3.96), 
                                         ('org3',  32.3), 
@@ -22,7 +24,12 @@ class TestSelection(unittest.TestCase):
         them to a population that becomes the next generation
         It repeats more tournaments until the next generation reaches the population size
         """
-        pass
+        organism_a = pd_org.PDOrg(pd_org.MemoryPDGenotype(1,[True, False], [False]))
+        organism_b = pd_org.PDOrg(pd_org.MemoryPDGenotype(2,[True, False, False, True], [False, True]))
+        organisms = [organism_a, organism_b]
+        result = pd_selection.get_next_generation_by_selection(organisms)
+        self.assertEqual(len(organisms),len(result))
+        self.assertTrue(set(organisms) >= set(result))
         
 if __name__ == "__main__":
     unittest.main()
