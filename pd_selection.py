d7507d5967ab9695355d3034a60d1755c83931d9
Mikaela Leas <mikaela.leas@gmail.com>
2015-06-05 10:56:59 -0400

    Updated next_generation function and test

--- pd_selection.py
+++ pd_selection.py
@@ -15,7 +15,6 @@ def get_best_half(paired_organism_payouts):
 def get_next_generation_by_selection(organisms):
     """
     Get next generation by selection is a function that takes a list of organisms
-    
     Shuffle the organisms and group them into TOURNAMENT_SIZEd clumps
     For a given tournament size, it runs a tournament
     selects the best half
@@ -25,23 +24,27 @@ def get_next_generation_by_selection(organisms):
     It repeats more tournaments until the next generation reaches the population size
     """
     def generate_contenders(organisms):
-        random.shuffle(organisms)
         number_of_tournaments = len(organisms) // TOURNAMENT_SIZE
         if not len(organisms) % TOURNAMENT_SIZE:
             number_of_tournaments += 1
-        for i in range(number_of_tournaments):
-            organisms[TOURNAMENT_SIZE * i: TOURNAMENT_SIZE * (i + 1)]
-            
-            
-            
-            
+        
+        while True:
+            random.shuffle(organisms)
+            for i in range(number_of_tournaments):
+                yield organisms[TOURNAMENT_SIZE * i: TOURNAMENT_SIZE * (i + 1)]         
+                   
     next_generation = []
     #function that adds things to next generation
+    contender_generator = generate_contenders(organisms)
+    
     while len(next_generation) < len(organisms):
         #pick organisms for the tournament
-        
+        contenders = next(contender_generator)
         #get the winners of the tournament
+        paired_org_payouts = list(pd_tournament.get_average_payouts(contenders))
+        winners = get_best_half(paired_org_payouts)
         #add winners to the next generation
+        next_generation += winners
     #make next_generation same length as organisms
     return next_generation[:len(organisms)]
     
